18d4976d3a70b222248a4d29eca925c307f48e7b
tumuka <umutak01@gmail.com>
2025-04-28 15:32:38 +0300

    Update tesla_checker.py

--- tesla_checker.py
+++ tesla_checker.py
@@ -7,18 +7,21 @@ import os
 TELEGRAM_BOT_TOKEN = "8117324210:AAGUyfXfnUSmZDKhuvz4VrR0jxYFsnjZ69E"
 TELEGRAM_USER_ID = "6944382551"
 
+# Browserless WebSocket URL'in
+BROWSERLESS_WS_ENDPOINT = "wss://chrome.browserless.io?token=SDHJ6gIu79Ys4q06e3e4de0e2ffce5502462e479ae"
+
 # Tesla URL
 TESLA_INVENTORY_URL = "https://www.tesla.com/tr_TR/inventory/new/my?arrangeby=plh&zip=34000&range=0"
 
 async def check_tesla_inventory():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=True)
+        browser = await p.chromium.connect_over_cdp(BROWSERLESS_WS_ENDPOINT)
         page = await browser.new_page()
         await page.goto(TESLA_INVENTORY_URL)
-        await page.wait_for_timeout(5000)  # 5 saniye bekle, sayfa tam yüklensin
+        await page.wait_for_timeout(5000)
 
         content = await page.content()
-        
+
         if "No Inventory Available" in content or "Mevcut araç bulunamadı" in content:
             print("Stok bulunamadı.")
         else:
@@ -26,6 +29,7 @@ async def check_tesla_inventory():
             send_telegram_message("🚗 Tesla stokta araç bulundu! Hemen bak: " + TESLA_INVENTORY_URL)
 
         await browser.close()
+
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
